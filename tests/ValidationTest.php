f29d63c234d0480cd72b3d931fc0cd267bd7a4e7
AJ Quick <ajquick@gmail.com>
2017-05-12 00:02:18 -0600

    Reformat all code and update copyright.

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -1,27 +1,27 @@
 <?php
 /**
- *      __  ___      ____  _     ___                           _                    __
- *     /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
- *    / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ /
- *   / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /
- *  /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/
+ *     __  ___      ____  _     ___                           _                    __
+ *    /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
+ *   / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ /
+ *  / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /
+ * /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/
  *
- *  @author Multidimension.al
- *  @copyright Copyright © 2016-2017 Multidimension.al - All Rights Reserved
- *  @license Proprietary and Confidential
+ * @author Multidimension.al
+ * @copyright Copyright © 2016-2017 Multidimension.al - All Rights Reserved
+ * @license Proprietary and Confidential
  *
- *  NOTICE:  All information contained herein is, and remains the property of
- *  Multidimension.al and its suppliers, if any.  The intellectual and
- *  technical concepts contained herein are proprietary to Multidimension.al
- *  and its suppliers and may be covered by U.S. and Foreign Patents, patents in
- *  process, and are protected by trade secret or copyright law. Dissemination
- *  of this information or reproduction of this material is strictly forbidden
- *  unless prior written permission is obtained.
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Multidimension.al and its suppliers, if any.  The intellectual and
+ * technical concepts contained herein are proprietary to Multidimension.al
+ * and its suppliers and may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained.
  */
 
 namespace Multidimensional\ArrayValidation\Test;
 
-use \Exception;
+use Exception;
 use Multidimensional\ArrayValidation\Validation;
 use PHPUnit\Framework\TestCase;
 
@@ -240,7 +240,7 @@ class ValidationTest extends TestCase
     {
         $rules = [
             'a' => ['type' => 'string', 'pattern' => '[A-Z]{2}'],
-            'b' => ['type' => 'datetime', 'pattern' => 'ISO 8601' ]
+            'b' => ['type' => 'datetime', 'pattern' => 'ISO 8601']
         ];
         $array = [
             'a' => 'CA',
@@ -329,7 +329,7 @@ class ValidationTest extends TestCase
             'a' => [
                 'type' => 'group',
                 'fields' => $rules2
-                ]
+            ]
         ];
         $array = [
             'a' => [['b' => 'Hello']]
